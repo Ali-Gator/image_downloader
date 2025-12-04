2d5a19e33b65cc3a94075f28b88b6b33ed0e4d2f
Oleg G <ogordienov@yandex.ru>
2025-12-04 08:27:37 +0200

    filter errors

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -217,8 +217,7 @@ export const ContentScriptConstants = {
 
 export const SENTRY_FILTER_ERRORS = [
   // Too generic: replace with precise Chrome messaging cases
-  'cannot access contents of',
-  'cannot access a chrome',
+  'cannot access',
   'extensions gallery',
   'frame with id 0',
   'no tab with id:',
